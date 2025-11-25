31f44716c0275178a0b11b340d27d4e81c9d2b70
DavidB <david.bernard@fms-ea.com>
2025-11-25 09:44:54 +0100

    foreign key added and prefixes

--- 25_11/garage.sql
+++ 25_11/garage.sql
@@ -1,15 +1,16 @@
-CREATE TABLE voiture (
-    id INT PRIMARY KEY,
-    marque VARCHAR(50),
-    type VARCHAR(50),
-    energie VARCHAR(50),
+CREATE TABLE g_voiture (
+    v_id INT PRIMARY KEY,
+    v_marque VARCHAR(50),
+    v_type VARCHAR(50),
+    v_energie VARCHAR(50),
+    FOREIGN KEY (v_fk_article_id) REFERENCES article(id)
 );
 
 
-CREATE TABLE article (
-    id INT PRIMARY KEY,
-    reference VARCHAR(50),
-    type VARCHAR(50),
-    marque VARCHAR(50),
-    quantitee INT,
+CREATE TABLE g_article (
+    a_id INT PRIMARY KEY,
+    a_reference VARCHAR(50),
+    a_type VARCHAR(50),
+    a_marque VARCHAR(50),
+    a_quantitee INT,
 )
