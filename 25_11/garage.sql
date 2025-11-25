965be07f1c6cb96288be3e6156de2084fc7970ff
DavidB <david.bernard@fms-ea.com>
2025-11-25 10:47:57 +0100

    alter corrected

--- 25_11/garage.sql
+++ 25_11/garage.sql
@@ -6,17 +6,17 @@ CREATE TABLE g_voiture (
     v_marque VARCHAR(50) NOT NULL,
     v_type VARCHAR(50) NOT NULL,
     v_energie VARCHAR(50) NOT NULL,
-    v_fk_article_id INT,
-    FOREIGN KEY (v_fk_article_id) REFERENCES article(id)
 ) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
 
 
+ALTER TABLE g_voiture ADD CONSTRAINT v_fk_article_id FOREIGN KEY (v_fk_article_id) REFERENCES g_article(a_id);
+
 -- Structure de la table article
 
-CREATE TABLE g_article CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci(
+CREATE TABLE g_article (
     a_id INT PRIMARY KEY,
     a_reference VARCHAR(50) NOT NULL,
     a_type VARCHAR(50) NOT NULL,
     a_marque VARCHAR(50) NOT NULL,
     a_quantitee INT NOT NULL,
-)
+) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
