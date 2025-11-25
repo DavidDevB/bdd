bb9b33fcae1537e7cf99751c334c00e0527bd3c2
DavidB <david.bernard@fms-ea.com>
2025-11-25 10:35:03 +0100

    utf8 added to create tables

--- 25_11/garage.sql
+++ 25_11/garage.sql
@@ -1,16 +1,21 @@
-CREATE TABLE g_voiture (
+
+-- Structure de la table voiture
+
+CREATE TABLE g_voiture CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci (
     v_id INT PRIMARY KEY,
-    v_marque VARCHAR(50),
-    v_type VARCHAR(50),
-    v_energie VARCHAR(50),
+    v_marque VARCHAR(50) NOT NULL,
+    v_type VARCHAR(50) NOT NULL,
+    v_energie VARCHAR(50) NOT NULL,
     FOREIGN KEY (v_fk_article_id) REFERENCES article(id)
 );
 
 
-CREATE TABLE g_article (
+-- Structure de la table article
+
+CREATE TABLE g_article CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci(
     a_id INT PRIMARY KEY,
-    a_reference VARCHAR(50),
-    a_type VARCHAR(50),
-    a_marque VARCHAR(50),
-    a_quantitee INT,
+    a_reference VARCHAR(50) NOT NULL,
+    a_type VARCHAR(50) NOT NULL,
+    a_marque VARCHAR(50) NOT NULL,
+    a_quantitee INT NOT NULL,
 )
