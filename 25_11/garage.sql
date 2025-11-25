9676dab12671f189b8fb95a02cabd9e073cb8797
DavidB <david.bernard@fms-ea.com>
2025-11-25 12:40:06 +0100

    bdd up to date

--- 25_11/garage.sql
+++ 25_11/garage.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : mar. 25 nov. 2025 à 10:36
+-- Généré le : mar. 25 nov. 2025 à 11:39
 -- Version du serveur : 8.4.7
 -- Version de PHP : 8.3.28
 
@@ -29,19 +29,19 @@ SET time_zone = "+00:00";
 
 DROP TABLE IF EXISTS `g_article`;
 CREATE TABLE IF NOT EXISTS `g_article` (
-  `a_id` int DEFAULT NULL,
+  `a_id` int NOT NULL AUTO_INCREMENT,
   `a_reference` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `a_type` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `a_marque` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `a_quantitee` int DEFAULT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  `a_quantitee` int DEFAULT NULL,
+  PRIMARY KEY (`a_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Déchargement des données de la table `g_article`
 --
 
 INSERT INTO `g_article` (`a_id`, `a_reference`, `a_type`, `a_marque`, `a_quantitee`) VALUES
-(1, 'Renault', 'Pneu', '1H765J87', 0),
 (1, '1H765J87', 'Essuie-glace', 'Renault', 1),
 (2, '9KJ76H5', 'Pneu', 'Peugeot', 1),
 (3, '76JH65K', 'Essuie-glace', 'Citroen', 1),
@@ -49,7 +49,9 @@ INSERT INTO `g_article` (`a_id`, `a_reference`, `a_type`, `a_marque`, `a_quantit
 (5, 'JH76KJ8', 'Pneu', 'BMW', 1),
 (6, '87H65KJ', 'Essuie-glace', 'Audi', 1),
 (7, 'KJ87H65', 'Filtre à air', 'Mercedes', 1),
-(8, 'H65KJ87', 'Filtre à air', 'Volkswagen', 1);
+(8, 'H65KJ87', 'Filtre à air', 'Volkswagen', 1),
+(9, '8JH6F45', 'Pneu', 'Dacia', 1),
+(10, 'L9J765F', 'Rétroviseur', 'Jaguar', 1);
 
 -- --------------------------------------------------------
 
@@ -59,13 +61,14 @@ INSERT INTO `g_article` (`a_id`, `a_reference`, `a_type`, `a_marque`, `a_quantit
 
 DROP TABLE IF EXISTS `g_voiture`;
 CREATE TABLE IF NOT EXISTS `g_voiture` (
-  `v_id` int DEFAULT NULL,
+  `v_id` int NOT NULL AUTO_INCREMENT,
   `v_marque` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `v_type` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `v_energie` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `v_fk_article_id` int DEFAULT NULL,
+  PRIMARY KEY (`v_id`),
   KEY `article_fk` (`v_fk_article_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Déchargement des données de la table `g_voiture`
@@ -80,7 +83,6 @@ INSERT INTO `g_voiture` (`v_id`, `v_marque`, `v_type`, `v_energie`, `v_fk_articl
 (6, 'Audi', 'SUV', 'Diesel', 6),
 (7, 'Mercedes', 'Berline', 'Essence', 7),
 (8, 'Volkswagen', 'Compacte', 'Diesel', 8);
-
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
